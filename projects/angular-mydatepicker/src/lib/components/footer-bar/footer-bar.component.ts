95d963e9eba0c7ca5eb59bed4d5d38d70d3653f9
= <104793781+gomarcello@users.noreply.github.com>
2024-10-07 23:30:43 -0400

    create 15 version

--- projects/angular-mydatepicker/src/lib/components/footer-bar/footer-bar.component.ts
+++ projects/angular-mydatepicker/src/lib/components/footer-bar/footer-bar.component.ts
@@ -1,13 +1,21 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewEncapsulation} from "@angular/core";
-import {IMyOptions} from "../../interfaces/my-options.interface";
-import {UtilService} from "../../services/angular-mydatepicker.util.service";
-import {OPTS, SPACE_STR, EMPTY_STR} from "../../constants/constants";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewEncapsulation,
+} from "@angular/core";
+import { EMPTY_STR, OPTS, SPACE_STR } from "../../constants/constants";
+import { IMyOptions } from "../../interfaces/my-options.interface";
+import { UtilService } from "../../services/angular-mydatepicker.util.service";
 
 @Component({
   selector: "lib-footer-bar",
   templateUrl: "./footer-bar.component.html",
   providers: [UtilService],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class FooterBarComponent implements OnChanges {
   @Input() opts: IMyOptions;
@@ -15,21 +23,23 @@ export class FooterBarComponent implements OnChanges {
 
   footerBarTxt: string = EMPTY_STR;
 
-  constructor(private utilService: UtilService) { }
+  constructor(private utilService: UtilService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.hasOwnProperty(OPTS)) {
       this.opts = changes[OPTS].currentValue;
 
-      const {dateFormat, monthLabels, todayTxt} = this.opts;
+      const { dateFormat, monthLabels, todayTxt } = this.opts;
 
       const today = this.utilService.getToday();
-      this.footerBarTxt = todayTxt + (todayTxt.length > 0 ? SPACE_STR : EMPTY_STR) + 
+      this.footerBarTxt =
+        todayTxt +
+        (todayTxt.length > 0 ? SPACE_STR : EMPTY_STR) +
         this.utilService.formatDate(today, dateFormat, monthLabels);
     }
   }
 
   onFooterBarTxtClicked(): void {
-    this.footerBarTxtClicked.emit()
+    this.footerBarTxtClicked.emit();
   }
 }
