d16b117e4c63c17527571c3c4cff34ce696f6b83
= <104793781+gomarcello@users.noreply.github.com>
2024-10-07 22:41:51 -0400

    upgrade to 11

--- projects/angular-mydatepicker/src/lib/spec/angular-mydatepicker.component.spec.ts
+++ projects/angular-mydatepicker/src/lib/spec/angular-mydatepicker.component.spec.ts
@@ -1,5 +1,5 @@
 import { Component, DebugElement, ViewChild } from "@angular/core";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { FormsModule } from "@angular/forms";
 import { By } from "@angular/platform-browser";
 import { AngularMyDatePickerDirective } from "../angular-mydatepicker.input";
@@ -100,7 +100,7 @@ class AngularMyDatepickerTestComponent {
 }
 
 describe("AngularMyDatePickerComponent", () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AngularMyDatepickerTestComponent],
       imports: [FormsModule, AngularMyDatePickerModule],
